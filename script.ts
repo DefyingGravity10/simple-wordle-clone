8772591d6133a7459b83f083b998aa66d4069242
Jeanne <jeanneclarissetoledo@gmail.com>
2022-09-25 17:14:39 +0800

    Implement wordless Wordle game; got past checkpoint 2

--- script.ts
+++ script.ts
@@ -1,4 +1,6 @@
 const url = "https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/ca9018b32e963292473841fb55fd5a62176769b5/valid-wordle-words.txt";
+let numberOfGuesses = 0;
+let wordToGuess = "";
 
 function startingConfig() {
     const div = document.getElementById("start-div");
@@ -22,12 +24,62 @@ function startingConfig() {
                 xhr.onload = function() {
                     const arr = xhr.responseText.split('\n');
                     const randomNumber: number = Math.floor(Math.random() * arr.length); 
-                    console.log(arr[randomNumber]);
+                    wordToGuess = arr[randomNumber];
+                    setMainScreen(wordToGuess);
                 };
                 xhr.send();
             }
         });
     } 
+}
+
+function setMainScreen(wordToGuess: string) {
+    const div = document.getElementById("start-div");
+    if (div !== null) {
+        //Create new screen
+        const userGuess = document.createElement("input");
+        userGuess.classList.add("user-guess");
+
+        const letterDict = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+                            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                            "u", "v", "w", "x", "y", "z"];
+        const letterContainer = document.createElement("div");
+        letterContainer.classList.add("container");
+        for (let i=0; i<26; i++) {
+            const letter = document.createElement("span");
+            letter.classList.add("letter");
+            letter.textContent = letterDict[i];
+            letterContainer.appendChild(letter);
+        }
+        div.replaceChildren(userGuess, letterContainer);
+
+        console.log(wordToGuess)
+        //Obtain user guesses
+        userGuess.addEventListener("keydown", verifyGuess);
+    }    
+}
+
+function verifyGuess(e: KeyboardEvent) {
+    const userGuess = document.getElementsByTagName("input").item(0);
+    if (e.key === "Enter" && userGuess !== null) {
+        if (userGuess.value.length !== 5) {
+            alert("The input box should have exactly 5 characters!");
+        }
+        else {
+            numberOfGuesses += 1;
+            const newGuess = document.createElement("p");
+            newGuess.textContent = userGuess.value;
+
+            if (userGuess.value.toLowerCase() === wordToGuess.toLowerCase()) {
+                alert("You guessed the correct word!");
+                userGuess.removeEventListener("keydown", verifyGuess);
+            }
+            else if (numberOfGuesses === 6){
+                alert(`Sorry, you lose. You already used up all 6 guesses.\nThe correct word is ${wordToGuess}!`);
+                userGuess.removeEventListener("keydown", verifyGuess);
+            }
+        }
+    }
 }
 
 startingConfig();
